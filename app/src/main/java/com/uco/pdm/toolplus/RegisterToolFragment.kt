83b2b3fdfb48d5bd319fff2929e918fdaf57de0f
CEIBA\santiago.cardenas <santiago.cardenas@ceiba.com.co>
2023-02-28 10:32:42 -0500

    Adjustments of some classes and xml files

--- app/src/main/java/com/uco/pdm/toolplus/RegisterToolFragment.kt
+++ app/src/main/java/com/uco/pdm/toolplus/RegisterToolFragment.kt
@@ -1,25 +1,22 @@
 package com.uco.pdm.toolplus
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.airbnb.lottie.LottieAnimationView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RegisterToolFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RegisterToolFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    lateinit var btnRegister: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -34,26 +31,55 @@ class RegisterToolFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register_tool, container, false)
+        val view:View = inflater.inflate(R.layout.fragment_register_tool, container, false)
+
+        btnRegister = view.findViewById(R.id.addRegister)
+        btnRegister.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(p0: View?) {
+                dialogPersonalized()
+            }
+
+        })
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegisterToolFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegisterToolFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun dialogPersonalized(){
+        val builder:AlertDialog.Builder = AlertDialog.Builder(activity)
+        val inflater:LayoutInflater = layoutInflater
+        val view:View = inflater.inflate(R.layout.dialog_personalized, null)
+
+        builder.setView(view)
+
+        val dialog:AlertDialog = builder.create()
+        dialog.show()
+
+        val btnYes:LottieAnimationView = view.findViewById(R.id.btnYes)
+        btnYes.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(p0: View?) {
+                //Realiza tarea
+                dialog.dismiss()
             }
+        })
+
+        val btnNo:LottieAnimationView = view.findViewById(R.id.btnNo)
+        btnNo.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(p1: View?) {
+                //Realiza tarea
+                dialog.dismiss()
+            }
+        })
     }
+
+    /*private fun animation(imageView: LottieAnimationView, animation: Int, action:Boolean){
+        if (action){
+            imageView.setAnimation(animation)
+            imageView.playAnimation()
+        }else{
+            imageView.setAnimation(animation)
+            imageView.playAnimation()
+        }
+    }*/
+
+
 }
