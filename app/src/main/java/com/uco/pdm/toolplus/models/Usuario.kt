f46056f4df8c66ff3e0a5f3d2305399f4613c32f
CEIBA\santiago.cardenas <santiago.cardenas@ceiba.com.co>
2023-04-26 18:31:15 -0500

    Se intenta realizar base de datos

--- app/src/main/java/com/uco/pdm/toolplus/models/Usuario.kt
+++ app/src/main/java/com/uco/pdm/toolplus/models/Usuario.kt
@@ -6,13 +6,11 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "tabla_usuarios")
 data class Usuario(
+    @PrimaryKey val correo: String,
     @ColumnInfo(name = "nombre") var nombre: String? = null,
     @ColumnInfo(name = "cedula") var cedula: String? = null,
     @ColumnInfo(name = "celular") var celular: String? = null,
     @ColumnInfo(name = "direccion") var direccion: String? = null,
     @ColumnInfo(name = "password") var password: String? = null,
-    @ColumnInfo(name = "decision") var decision: Boolean? = null
-){
-    @PrimaryKey(autoGenerate = false)
-    var correo: String = ""
-}
+    @ColumnInfo(name = "decision") var decision: String? = null
+)
--- app/src/main/java/com/uco/pdm/toolplus/vista/userRegister/user_register.kt
+++ app/src/main/java/com/uco/pdm/toolplus/vista/userRegister/user_register.kt
@@ -1,18 +1,26 @@
 package com.uco.pdm.toolplus.vista.userRegister
 
 import android.os.Bundle
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
 import com.uco.pdm.toolplus.R
 import com.uco.pdm.toolplus.databinding.FragmentSevenRegUserBinding
+import com.uco.pdm.toolplus.models.Usuario
+import com.uco.pdm.toolplus.persistence.database.AppDatabase
 
 class user_register : Fragment() {
 
     private var _binding: FragmentSevenRegUserBinding? = null
 
     private val binding get() = _binding!!
+    private lateinit var db: AppDatabase
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,12 +28,32 @@ class user_register : Fragment() {
         }
     }
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentSevenRegUserBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
+    {
+        super.onViewCreated(view, savedInstanceState)
+        db = AppDatabase.getInstance(context)
+        binding.buttonCreate.setOnClickListener {view ->
+            Toast.makeText(context, "Usuario creado", Toast.LENGTH_LONG).show()
+            val usuario = Usuario(
+                binding.editTextTextEmailAddress2.toString(),
+                binding.editTextTextPersonName.toString(),
+                binding.editTextNumber.toString(),
+                binding.editTextPhone.toString(),
+                binding.editTextTextPostalAddress.toString(),
+                binding.editTextTextPassword2.toString(),
+                binding.editTextTextPersonName2.toString()
+            )
+            db.usuarioDAO().insertAll(usuario)
+        }
     }
 
     companion object {
@@ -44,5 +72,13 @@ class user_register : Fragment() {
                 arguments = Bundle().apply {
                 }
             }
+    }
+
+    private fun CharSequence?.isValidEmail() =
+        !isNullOrEmpty() && Patterns.EMAIL_ADDRESS.matcher(this).matches()
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
