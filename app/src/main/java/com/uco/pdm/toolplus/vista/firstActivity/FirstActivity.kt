bd73320ab69af7d42fb0447ac66795dac04c1d63
CEIBA\santiago.cardenas <santiago.cardenas@ceiba.com.co>
2023-05-24 19:06:04 -0500

    Arreglos de firebase

--- app/src/main/java/com/uco/pdm/toolplus/vista/firstActivity/FirstActivity.kt
+++ app/src/main/java/com/uco/pdm/toolplus/vista/firstActivity/FirstActivity.kt
@@ -2,18 +2,14 @@ package com.uco.pdm.toolplus.vista.firstActivity
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
-import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.button.MaterialButton
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
 import com.uco.pdm.toolplus.R
 import com.uco.pdm.toolplus.databinding.ActivityFirstBinding
-import com.uco.pdm.toolplus.databinding.FragmentSevenRegUserBinding
 import com.uco.pdm.toolplus.models.Usuario
 import com.uco.pdm.toolplus.persistence.database.AppDatabase
 import com.uco.pdm.toolplus.vista.fragmentSevenRegUser.Fragment_Seven_Reg_User
@@ -24,6 +20,9 @@ class FirstActivity : AppCompatActivity() {
     private var _binding: ActivityFirstBinding? = null
     private val binding get() = _binding!!
     private lateinit var db: AppDatabase
+    private lateinit var user: EditText
+    private lateinit var pass: EditText
+    private lateinit var auth: FirebaseAuth
 
 
     var email = ""
@@ -36,11 +35,9 @@ class FirstActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         db = AppDatabase.getInstance(this)
-
-
-
-
-
+        user = findViewById(R.id.edusername)
+        pass = findViewById(R.id.edcontra)
+        auth = FirebaseAuth.getInstance()
 
         /*binding.btnentrar.setOnClickListener{
             val user: EditText = findViewById(R.id.edusername)
@@ -70,28 +67,44 @@ class FirstActivity : AppCompatActivity() {
 
     fun entrar(view: View){
         db = AppDatabase.getInstance(this)
-        val user: EditText = findViewById(R.id.edusername)
-        val pass: EditText = findViewById(R.id.edcontra)
+        val user:String= user.text.toString()
+        val pass:String= pass.text.toString()
+        val userDB: Usuario = db.usuarioDAO().getUser(user, pass)
+        if(!TextUtils.isEmpty(user) && !TextUtils.isEmpty(pass) ){
+            auth.signInWithEmailAndPassword(user, pass)
+                .addOnCompleteListener(this){
+                    task ->
+                    if(task.isSuccessful){
+                        println("User "+task.result)
 
-        val userDB: Usuario = db.usuarioDAO().getUser(user.text.toString(), pass.text.toString())
 
-        if(userDB != null && userDB.decision == "admin"){
-            Toast.makeText(this, "Usuario encontrado", Toast.LENGTH_LONG).show()
-            val intent = Intent(this, RecyclerViewToolsActivity::class.java).apply {
-                putExtra("EMAIL", userDB.correo)
-            }
-            startActivity(intent)
+                        if(userDB != null && userDB.decision == "admin"){
+                            Toast.makeText(this, "Usuario encontrado", Toast.LENGTH_LONG).show()
+                            val intent = Intent(this, RecyclerViewToolsActivity::class.java).apply {
+                                putExtra("EMAIL", userDB.correo)
+                            }
+                            startActivity(intent)
+                        }
+                        else if(userDB != null && userDB.decision == "cliente"){
+                            Toast.makeText(this, "Usuario encontrado", Toast.LENGTH_LONG).show()
+                            val intent = Intent(this, ReciclerViewToolUserActivity::class.java).apply {
+                                putExtra("EMAIL", userDB.correo)
+                            }
+                            startActivity(intent)
+                        }
+                    }
+                    else{
+                        Toast.makeText(this, "Error en la auth", Toast.LENGTH_LONG).show()
+                        if (userDB == null){
+                            Toast.makeText(this, "Usuario no encontrado", Toast.LENGTH_LONG).show()
+                        }
+                    }
+                }
         }
-        else if(userDB != null && userDB.decision == "cliente"){
-            Toast.makeText(this, "Usuario encontrado", Toast.LENGTH_LONG).show()
-            val intent = Intent(this, ReciclerViewToolUserActivity::class.java).apply {
-                putExtra("EMAIL", userDB.correo)
-            }
-            startActivity(intent)
-        }
-        else if (userDB == null){
-            Toast.makeText(this, "Usuario no encontrado", Toast.LENGTH_LONG).show()
-        }
+
+
+
+
 
     }
 }
--- app/src/main/java/com/uco/pdm/toolplus/vista/fragmentSevenRegUser/Fragment_Seven_Reg_User.kt
+++ app/src/main/java/com/uco/pdm/toolplus/vista/fragmentSevenRegUser/Fragment_Seven_Reg_User.kt
@@ -2,6 +2,7 @@ package com.uco.pdm.toolplus.vista.fragmentSevenRegUser
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +10,10 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.uco.pdm.toolplus.R
 import com.uco.pdm.toolplus.databinding.FragmentSevenRegUserBinding
 import com.uco.pdm.toolplus.models.Usuario
@@ -20,6 +25,19 @@ class Fragment_Seven_Reg_User : Fragment() {
     private var _binding: FragmentSevenRegUserBinding? = null
     private val binding get() = _binding!!
     private lateinit var db: AppDatabase
+
+    private lateinit var txtName:EditText
+    private lateinit var idNumber:EditText
+    private lateinit var cellNumber:EditText
+    private lateinit var txtAddres:EditText
+    private lateinit var txtEmail:EditText
+    private lateinit var txtPassword:EditText
+    private lateinit var txtValidate:EditText
+    private lateinit var database: FirebaseDatabase
+    private lateinit var dbreference: DatabaseReference
+    private lateinit var auth: FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -30,12 +48,25 @@ class Fragment_Seven_Reg_User : Fragment() {
     ): View? {
 
         _binding = FragmentSevenRegUserBinding.inflate(inflater, container, false)
+
+        txtName = binding.root.findViewById(R.id.editTextTextPersonName)
+        idNumber = binding.root.findViewById(R.id.editTextNumber)
+        cellNumber = binding.root.findViewById(R.id.editTextPhone)
+        txtAddres = binding.root.findViewById(R.id.editTextTextPostalAddress)
+        txtEmail = binding.root.findViewById(R.id.editTextTextEmailAddress2)
+        txtPassword = binding.root.findViewById(R.id.editTextTextPassword2)
+        txtValidate = binding.root.findViewById(R.id.editTextTextPersonName2)
         crear(binding.root)
         val returnLogin = binding.root.findViewById<Button>(R.id.button2)
         returnLogin.setOnClickListener {
             val intent = Intent(activity, FirstActivity::class.java)
             startActivity(intent)
         }
+
+        database= FirebaseDatabase.getInstance()
+        auth= FirebaseAuth.getInstance()
+
+        dbreference=database.reference.child("User")
         return binding.root
     }
 
@@ -44,6 +75,7 @@ class Fragment_Seven_Reg_User : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         db = AppDatabase.getInstance(context)
         binding.buttonCreate.setOnClickListener {
+            crearNuevaCuenta()
             val usuario = Usuario(
                 binding.editTextTextEmailAddress2.text.toString(),
                 binding.editTextTextPersonName.text.toString(),
@@ -76,6 +108,60 @@ class Fragment_Seven_Reg_User : Fragment() {
             }
             startActivity(intent)
         }
+    }
+
+    private fun crearNuevaCuenta (){
+
+        val name: String=txtName.text.toString()
+        val id: String=idNumber.text.toString()
+        val cel: String=cellNumber.text.toString()
+        val add: String= txtAddres.text.toString()
+        val email:String=txtEmail.text.toString()
+        val pasw: String=txtPassword.text.toString()
+        val valid:String=txtValidate.text.toString()
+
+        if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(id) && !TextUtils.isEmpty(cel) && !TextUtils.isEmpty(add) && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(pasw) && !TextUtils.isEmpty(valid)){
+
+            auth.createUserWithEmailAndPassword(email,pasw)
+                .addOnCompleteListener{
+                        task ->
+                    if (task.isComplete){
+                        println("Task Complete " + task.result)
+                        val user: FirebaseUser?=auth.currentUser
+                        println("User es " + user)
+                        verifyEmail(user)
+                        val userBD=dbreference.child(user?.uid.toString())
+                        userBD.child("name").setValue(name)
+                        userBD.child("id").setValue(id)
+                        userBD.child("cel").setValue(cel)
+                        userBD.child("add").setValue(add)
+                        userBD.child("email").setValue(email)
+                        userBD.child("pasw").setValue(pasw)
+                        userBD.child("valid").setValue(valid)
+                        action()
+
+                    }
+
+                }
+            Toast.makeText(context, "Usuario creado firebase", Toast.LENGTH_LONG).show()
+
+        }
+    }
+
+    private fun action(){
+        startActivity(Intent(requireContext().applicationContext,FirstActivity::class.java))
+    }
+
+    private fun verifyEmail(user: FirebaseUser?){
+        user?.sendEmailVerification()
+            ?.addOnCompleteListener{
+                    task ->
+                if(task.isComplete){
+                    Toast.makeText(requireContext().applicationContext, "Correo enviado", Toast.LENGTH_LONG).show()
+                }else{
+                    Toast.makeText(requireContext().applicationContext, "Error al enviar correo", Toast.LENGTH_LONG).show()
+                }
+            }
     }
 
 
